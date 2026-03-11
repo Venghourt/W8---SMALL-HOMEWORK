870f73d61ca622536a1daefc36b47db0e108887e
Chhoeun Venghourt <hourt2133@gmail.com>
2026-03-11 17:13:19 +0700

    class activity

--- lib/data/repositories/songs/song_repository_mock.dart
+++ lib/data/repositories/songs/song_repository_mock.dart
@@ -40,18 +40,23 @@ class SongRepositoryMock implements SongRepository {
   @override
   Future<List<Song>> fetchSongs() async {
     await Future.delayed(Duration(minutes: 2), () {});
- 
+
     return _songs;
   }
 
   @override
   Future<Song?> fetchSongById(String id) async {
     // - Simulate a delay of 3 seconds.
-
+    await Future.delayed(Duration(seconds: 3));
     // - After the delay : Find the song of given id in the list of songs and return it
-
     // - If not found : Throw an error with the message “no song found for id 25 in the database"
+    try {
+      final song = _songs.firstWhere((s) => s.id == id);
+      return song;
+    } catch (e) {
+      throw ("no song found for id 25 in the database");
+    }
 
-    return Future.delayed(Duration.zero); // TO CHANGE !
+    // return Future.delayed(Duration.zero); // TO CHANGE !
   }
 }
--- lib/test/song_repository_test.dart
+++ lib/test/song_repository_test.dart
@@ -1,10 +1,24 @@
- 
+import 'package:smallhomework/data/repositories/songs/song_repository_mock.dart';
+
 void main() async {
   //   Instantiate the  song_repository_mock
- 
+  final repo = SongRepositoryMock();
   // Test both the success and the failure of the post request
- 
   // Handle the Future using 2 ways  (2 tests)
   // - Using then() with .catchError().
+  repo
+      .fetchSongById("s1")
+      .then((song) {
+        print(song?.title);
+      })
+      .catchError((error) {
+        print(error);
+      });
   // - Using async/await with try/catch.
+  try {
+    var song = await repo.fetchSongById("s1");
+    print(song?.title);
+  } catch (error) {
+    print(error);
+  }
 }
